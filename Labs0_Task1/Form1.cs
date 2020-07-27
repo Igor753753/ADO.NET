6a80ac2ee569a81328f759c16a633f1a81deb41f
Igor <amfispb@yandex.ru>
2020-07-27 17:16:15 +0300

    commit

---
+++ Labs0_Task1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labs0_Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void courseBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.courseBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.schoolDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "schoolDataSet.OnlineCourse". При необходимости она может быть перемещена или удалена.
+            this.onlineCourseTableAdapter.Fill(this.schoolDataSet.OnlineCourse);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "schoolDataSet.Course". При необходимости она может быть перемещена или удалена.
+            this.courseTableAdapter.Fill(this.schoolDataSet.Course);
+
+        }
+    }
+}
